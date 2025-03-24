a421e8e0b1ccbd4abcfed4939f253317637f0fe0
Takhir <guvalakat1603@gmail.com>
2025-03-24 17:48:40 +0500

    Person

---
+++ src/main/java/uz/serializitionanddeserializition/Person.java
@@ -0,0 +1,39 @@
+package uz.serializitionanddeserializition;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String name;
+    private int age;
+    private transient String password;
+
+    public Person(String name, int age, String password) {
+        this.name = name;
+        this.age = age;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
