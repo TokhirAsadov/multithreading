0425bb0fcf6c0fa01c42a3b66a9184a18eee10d6
Takhir <guvalakat1603@gmail.com>
2025-03-25 11:28:37 +0500

    3-topshiriq

---
+++ src/main/java/uz/ioandnoi/homework/FileSearchUtility.java
@@ -0,0 +1,18 @@
+package uz.ioandnoi.homework;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+public class FileSearchUtility {
+    public static void searchInFile(String filePath, String keyword) throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            lines.filter(line -> line.contains(keyword))
+                    .forEach(System.out::println);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        searchInFile("error.log", "Logger thread");
+    }
+}
