0520e940716c0afa55e0f5a102ce773429c14d33
Takhir <guvalakat1603@gmail.com>
2025-03-27 10:20:27 +0500

    formatter is done for G52

--- src/main/java/uz/logging/TelegramAlarmFormatter.java
+++ src/main/java/uz/logging/TelegramAlarmFormatter.java
@@ -1,11 +1,26 @@
 package uz.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class TelegramAlarmFormatter extends Formatter {
     @Override
     public String format(LogRecord record) {
-        return "";
+        String stackTrace = "\n";
+        Throwable thrown = record.getThrown();
+        if ( thrown != null ){
+            StringWriter out = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(out);
+            thrown.printStackTrace(printWriter);
+            stackTrace += out.toString();
+        }
+        return "%s -- [%d] -- %s -- [%s] %s"
+                .formatted(record.getLevel(),
+                        record.getLongThreadID(),
+                        record.getLoggerName(),
+                        record.getMessage(),
+                        stackTrace);
     }
 }
