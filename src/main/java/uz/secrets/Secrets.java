9e8a64c4e07394daf58f30e46146b54413a5b188
Takhir <guvalakat1603@gmail.com>
2025-03-27 10:33:24 +0500

    clearing

--- src/main/java/uz/secrets/Secrets.java
+++
@@ -1,8 +0,0 @@
-package uz.secrets;
-
-public class Secrets {
-    public static final String CHAT_ID = "7567495333";
-    public static final String TOKEN = "8031796169:AAF5l7wsaTL65jL7CA1UJYCjg66fTN1md8s";
-    public static final String SEND_MESSAGE = "https://api.telegram.org/bot8031796169:AAF5l7wsaTL65jL7CA1UJYCjg66fTN1md8s/sendMessage";
-
-}
